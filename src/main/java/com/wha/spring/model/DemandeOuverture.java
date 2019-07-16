20d6261a533fbdb03d8092d0010a3d28f265a5a0
habibaHamdane <haboubalouaiy@gmail.com>
2019-07-16 16:30:48 +0200

    creation DemandeOuverture Dao Sevice Controller

--- src/main/java/com/wha/spring/model/DemandeOuverture.java
+++ src/main/java/com/wha/spring/model/DemandeOuverture.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -31,6 +32,7 @@ private ClientPotentiel clientPotentiel;
 @Column(name = "valide", nullable = false)
 private boolean valide;
 
+@ManyToOne
 private Conseiller conseiller;
 
 
